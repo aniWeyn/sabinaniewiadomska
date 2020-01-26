3bcb7883303b948b5b6250849b1f47560d5002ad
aniWeyn <aniWeyn@users.noreply.github.com>
2020-01-26 20:02:50 +0100

    Update ResumePL.vue
    updated content

--- sabina/src/views/ResumePL.vue
+++ sabina/src/views/ResumePL.vue
@@ -1,5 +1,79 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>Edukacja</h2>
+2003-2005 - Szkola Myzyczna Yamaha<br>
+2005-2007 - Panstwowa Szkola Muzyczna Ist, im L. Rozyckiego w Gliwicach - pianino<br>
+2005-2006 - Gliwicki Teat Muzyczny Junior<br>
+2007-2009 - Panstwowa Szkola Muzyczna IIst, im L. Rozyckiego w Gliwicach - wydzial wokalny<br>
+2009-2013 - prywatne lekcje Karolna Glazer / Dominika Plonka<br>
+2013-2014 - MDK, kolo wokalistow, nauczany przez certyfikowanego nauczyciela IVTOM<br>
+<h2>Warsztaty:</h2>
+2007 - Szlifiernia Warsztaty Aktorskie, 26.02. - 31.05.2007<br>
+2008 - IX Miedzynarodowy Kurs Wakacyjny Big-Bandow PSM I i II st. im W. Lutoslawskiego, w Nysie (13-19.07.2008)<br>
+2008 - Jaworki Warsztaty Muzyczne<br>
+Chodziez<br>
+2011 - Ogolnopolskie Warsztaty Wokalne - Miedzynarodowe Stowarzyszenie Sztuki Wokalnej Natural Voice Perfection, Gliwice, (8-10.04.2011)<br>
+2011 - Warsztaty Imporowizacji Wokalnych - Vocal Improvisation Techniques - Krakow, 11.2011<br>
+2012 - Ogolnopolskie Warsztaty Wokalne - Miedzynarodowe Stowarzyszenie Sztuki Wokalnej Natural Voice Perfection, Gliwice, (28-30.01.2012)<br>
+2012 - 42. Miedzynarodowe Warsztaty Jazzowe, Pulawy, 5-15.07.2012 <br>
+2014 - Warsztaty Muzyczne Jazz w Ruinach, w ramach 10 edycji festiwalu Jazz w Ruinach,<br> 
+2014 - Miedzynarodowe Warsztaty Wokalne - International IVTOM - Voice Teachers of Mix, Gliwice, (24-25.01.2014)<br>
+2014 - Jaworki Warsztaty Muzyczne<br>
+2014 - Warsztaty Wokalne Anny Serafinskiej - Letnie Gorace Ulice - Teat Syrena, 07/08.2014<br>
+<h2>Gliwicki Teatr Muzyczny - przedstawienia</h2>
+2006 - Gliwicki Teat Muzyczny Junior - udzial w przedstawieniu "Szmaragdowy Szlak" - 22.05.2006<br>
+2007 - Gliwicki Teat Muzyczny Junior - udzial w przedstawieniu ""<br>
+<h2>Konkursy</h2>
+2003 - wyroznienie I st. - II Festiwal Szkoly Muzycznej Yamaha w Gliwicach, solo,  (26.10.2003)<br>
+2005 - wyroznienie - II Miedzyszkolny Konkurs Piosenki Religijnej, solo,  (27.04.2005)<br>
+2005 - III miejsce - II Festiwal Szkoly Muzycznej Yamaha w Gliwicach, solo,  (11.12.2005)<br>
+2005 - I miejsce - II Festiwal Szkoly Muzycznej Yamaha w Gliwicach, duet,  (11.12.2005)<br>
+2006 - I miejsce w II Wojewodzkim Konkursie Piosenki Mlodzieiowej "Droga do gwiazd przez gimnazjum", Knurow - kategoria soliéci (06.04.2006)<br>
+2006 - I miejsce w Konkursie Powiatowym — VIII Prezentacje Artystyczne Dzieci i Mlodzieiy „Wiosenne granie i Spiewanie" w kategorii — solisci (21.03.2006)<br>
+2006 - Gliwicka Gala Mlodych Talentow Zwiastuny 2006 (07.06.2006), dyplom i statuetka za osiagniecia w dziedzinie artystycznej<br>
+2006 - I miejsce w Ill Wojewödzkim Konkursie Piosenki Mlodzieiowej „Droga do gwiazd przez gimnazjum" w kategorii — soliéci (13.10.2006)<br>
+2007 - II miejsce w Szkolnym Konkursie Piosneki Angielskiej, Gliwice (30.03.2007)<br>
+2007 - I miejsce w IV Wojew6dzkim Konkursie Piosenki Mlodzieiowej „Droga do gwiazd przez gimnazjum" dla Zespolu Wokalno — Instrumentalnego, Knurow (19.04.2007)<br>
+2007 - III miejsce w IV Wojew6dzkim Konkursie Piosenki Mlodzieiowej „Droga do gwiazd przez gimnazjum" - kategoria soliéci, Knurow (19.04.2007)<br>
+2007 - I miejsce, XII Festiwal Piosenki Angielskiej "Sing it for us...", kategoria solisci Zabrze (11.05.2007)<br>
+2007 - Szlifiernia Warsztaty Aktorskie, koncert, 31.05.2007<br>
+2007 - Gliwicka Gala Mlodych Talentow Zwiastuny 2007 (01.06.2007), dyplom i statuetka za osiagniecia w dziedzinie artystycznej<br>
+2007 - II miejsce w Konkursie Recytatorskim w zakresie tworczosci Jana Pawla III, Gliwice (17.10.2007)<br>
+2007 - I miejsce w powiatowym Miedzyszkolnym Konkursie „Hej kolqda, kolqda" w kategorii solist6w, Gliwice (21.12.2007)<br>
+2008 - I miejsce w powiatowym Turnieju Jednego Wiersza, Gliwice, (26.02.2008)<br>
+2008 - II miejsce w V Wojew6dzkim Konkursie Piosenki Mlodzieiowej „Droga do gwiazd przez gimnazjum", solista, Knurow (06.03.2008)<br>
+2008 - I miejsce w Wojew6dzkim Festiwalu Piosenki Harcerskiej i Turystycznej "...Leé, pieénileé, niesiona echem w dale.. " (03.2008)<br>
+2008 - III Miedzyszkolny Konkurs Literacki - Nie pisze do szuflady w kategorii poezja (29.04.2008)<br>
+2008 - III Miejsce w VIII Konkursie Recytatorskim w Ramach Polonistycznej Ligi Gimnazjalnej (06.06.2008)<br>
+2008 - Gliwicka Gala Mlodych Talentow Zwiastuny 2006 (19.06.2008), dyplom i statuetka za osiagniecia w dziedzinie artystycznej<br>
+2008 - II miejsce, Gliwicki Koncert Promocyjny, Gliwice (09.2008)<br>
+2008 - II miejsce, XV Festiwalu Piosenki Francuskiej, Gliwice, 26.11.2008<br>
+2009 - laureatka Talentu w powiatowym Wierszowaniu "Wywiedzione ze slowa - Zmyslowe dotykanie swiata", Gliwice (09.03.2009)<br>
+2009 - laureatka nagrody Talent Grand Prix w powiatowym Wierszowaniu "Wywiedzione ze slowa - Zmyslowe dotykanie swiata", Gliwice (09.03.2009)<br>
+2009 - I miejsce, I Gliwickie Konfrontacje Muzyczne, Gliwice, (18.03.2009)<br>
+2009 - I miejsce, solista, 34. Wojewodzkie Przeglady Szkolnych Zespolow Artystycznych, eliminacje, Piekary Slaskie, (21.04.2009)<br>
+2009 - I miejsce, solista, 34. Wojewodzkie Przeglady Szkolnych Zespolow Artystycznych, Czestochowa, (10.06.2009)<br>
+2009 - Gliwicka Gala Mlodych Talentow Zwiastuny 2009 (18.06.2009), dyplom i statuetka za osiagniecia w dziedzinie artystycznej<br>
+2009 - I miejsce w I Edycji Konkursu Poezji Spiewanej, (09.2009)<br>
+2010 - Grand Prix - XII Wojewodzkie Prezentacje Artystyczne Dzieci i Mlodziezy, Gliwice, (24.03.2010)<br>
+2010 - III miejsce, Turniej Poezji Spiewanej, 55. Oglnopolski konkurs Recytatorski, Gliwice eliminacje miejskie (03.2010)<br>
+2010 - I miejsce, II Gliwickie Konfrontacje Muzyczne, kategoria solisci, (08.02.2010)<br>
+2010 - I miejsce, w kategorii wokalnej, I Regionalny Konkurs Talentw Szkol Ponadgimnazjalnych - "Zlota Struna", Chorzow<br>
+2010 - II miejsce, w II Educja Konkursu Poeazji Spiewanej dla Szkol Ponadgimnazjalnych, Gliwice<br>
+2010 - wyroznienie, w Ogolnopolskim Festiwalu Piosenki Obcojezycznej Opatow, 24.04.2010, <br>
+2010 - srebrna polnutka, XIX Miedzynarodowy Festiwal Piosenki i Tanca - Majowa Nutka, Czestochowa, 16.05.2010<br>
+2010 - Gliwicka Gala Mlodych Talentow Zwiastuny 2010 (10.06.2010), dyplom i statuetka za osiagniecia w dziedzinie artystycznej<br>
+2011 - I miejsce, Konkurs Odkrywamy Talent, w kategorii muzyka, Gliwice, 15.02.2011<br>
+2011 - II miejsce, w XIII Ogolnopolskim Konkursie Piosenki Angielskiej, Nowy Targ, 23.02.2011<br>
+2011 - Gliwicka Gala Mlodych Talentow Zwiastuny 2011 (15.06.2011), dyplom i statuetka za osiagniecia w dziedzinie artystycznej<br>
+2012 - III miejsce - V Ogolnopolski Festiwal Piosenki Francuskiej, Brzeg, 21.10.2012<br>
+2012 - III miejsce - VI Festiwal Piosenki Niezapominajka, Rydultowy, 14.03.2014<br>
+
   </div>
 </template>
+
+<style>
+.about{
+  padding-left: 30px;
+}
+</style>

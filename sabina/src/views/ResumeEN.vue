fe465df987778b449a54c0c5b13675c3e1d0b3f0
aniWeyn <aniWeyn@users.noreply.github.com>
2020-01-26 20:03:56 +0100

    Update ResumeEN.vue
    update content

--- sabina/src/views/ResumeEN.vue
+++ sabina/src/views/ResumeEN.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    February 2019	Competition - solo vocalist	2nd place in student talent compentition in Gjøvik <br>
+August – December 2018	Choir	 I had a chance to participate in rehersals of Gjøvik Choir <br>
+April 2014	Theater – actress/solo vocalist	Musical “Tobiasz”, one of main character (singing role) <br>
+January – June 2014	Learning 	I won place as a one of seven students for sponsored classes of singing with MIX technique (SLS) <br>
+2012 - 2014	Vocal Association “MIX”	<br>
+14.03.2014	Competition - solo vocalist	3rd places in VI Festival of Songs “Niezapominajka”, Rydultowy <br>
+15.11.2013	Jazz Festival - volunteer	Volunteer at Palm Jazz Festiwal, Gliwice <br>
+19.10.2013	Concert	Solo vocalist at photo and painting vernissage, Gliwice <br>
+11.02.2013	Concert	Solo vocalist at event promoting health among youghts with cooperation with local tv – TVS, Gliwice <br>
+23.03.2013	Concert	Solo vocalist at concert promoting talented teenagers from Silesian region, Gliwice<br>
+24/25.01.2013	Workshops – participation and organization	Participation and co-organization of International Workshops of MIX Singing (modern commercial singing technique) with MIX Associations, Gliwice<br>
+2007	Competition - solo vocalist	1st place in Regional Festival of Scout and Turistic Songs “..Lec, piesni lec, niesiona w dal”<br>
+2007	Competition - solo vocalist	2nd place in V Regional Competition of Song “Droga do gwiazd przez gimnazjum”, Knurow<br>
+2006	Competition - solo vocalist	3rd place in IV Regional Competition of Song “Droga do gwiazd przez gimnazjum”, Knurow<br>
+2005	Competition - solo vocalist	1st place in III Regional Competition of Song “Droga do gwiazd przez gimnazjum”, Knurow <br>
+2005	Competition – instrumental and vocal band	2nd place in III Regional Competition of Song “Droga do gwiazd przez gimnazjum”, Knurow<br>
+
   </div>
 </template>

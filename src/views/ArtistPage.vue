cdcb54f06735722bef0cb4215bd738b342798fd1
aniWeyn <4niweyn@gmail.com>
2022-04-21 22:56:06 +0200

    kaziu

--- src/views/ArtistPage.vue
+++ src/views/ArtistPage.vue
@@ -4,7 +4,20 @@
     <span class="name header">Sabina</span>
     </div>
     <div class="column--right">
-        <span class="surname header">Niewiadomska</span></div>
+        <span class="surname header">Niewiadomska</span>
+    </div>
+    <section class="mp3">
+        <figure>
+    <audio
+        controls
+        src="../assets/media/Kaziu2.mp3">
+            Your browser does not support the
+            <code>audio</code> element.
+    </audio>
+     <figcaption>"Kaziu, zakochaj się" <br /> lyrics: Jeremi Przybora, music: Jerzy Wasowski</figcaption>
+</figure>
+    </section>
+
         <div class="teddy">
             <iframe loading="lazy" width="auto" height="auto" src="https://www.youtube-nocookie.com/embed/OF2U17HNOzs?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
     <div class="miloscPrawdziwa">
@@ -90,76 +103,81 @@ export default {
   justify-content: center;
 }
 
+.mp3{
+    grid-column: 2/4;
+  grid-row: 2;
+}
+
 .teddy{
   grid-column: 2;
-  grid-row: 2;
+  grid-row: 4;
 }
 
 
 .miloscPrawdziwa{
   grid-column: 3;
-  grid-row: 3;
+  grid-row: 5;
 }
 
 .adele{
   grid-column: 4;
-  grid-row: 4;
+  grid-row: 6;
 }
 
 .dworzec{
   grid-column: 2;
-  grid-row: 4;
+  grid-row: 7;
 }
 
 .swieta{
   grid-column: 2;
-  grid-row: 5;
+  grid-row: 7;
 }
 
 .greaterlove{
   grid-column: 3;
-  grid-row: 5;
+  grid-row: 7;
 }
 
 .pukpukpuk{
   grid-column: 2;
-  grid-row: 6;
+  grid-row: 8;
 }
 
 .cichywielbiciel{
   grid-column: 3;
-  grid-row: 6;
+  grid-row: 8;
 }
 
 .jessiej{
   grid-column: 4;
-  grid-row: 6;
+  grid-row: 8;
 }
 
 .malowiejskikrajobraz{
   grid-column: 2;
-  grid-row: 7;
+  grid-row: 9;
 }
 
 .deszcz{
   grid-column: 3;
-  grid-row: 8;
+  grid-row: 10;
 }
 
 
 .stop{
   grid-column: 4;
-  grid-row: 9;
+  grid-row: 11;
 }
 
 .tancbuda{
   grid-column: 2;
-  grid-row: 10;
+  grid-row: 12;
 }
 
 .timeaftertime{
   grid-column: 4;
-  grid-row: 10;
+  grid-row: 12;
 }
 
 .header{
